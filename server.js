92b6150a5209593dd86d34db9b25757741695535
winbigcoms <bigcoms6290@gmail.com>
2020-08-23 16:37:36 +0900

    db

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const app = express();
-let port = process.env.PORT || 9000;
+const cors = require('cors');
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
+let port = process.env.PORT || 9000;
+const mysql = require("mysql2/promise");
+const connectDb = mysql.createPool({
+  host: "us-cdbr-east-02.cleardb.com"
+  ,port: 3306
+  ,user : "ba26f111c136d8"
+  ,password : "efa96b0e"
+  ,database : "heroku_18c5f24897f4cf6"
+  ,connectionLimit: 10
+})
 app.get("/",(req,res)=> {
     res.send("jiji")
 });
