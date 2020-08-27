a8b9d48949d53c108c39a28bc0a6482645fd50f2
winbigcoms <bigcoms6290@gmail.com>
2020-08-27 14:35:29 +0900

    feat cors

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
  
-const corsOptions = {
-  origin: 'http://localhost:3000', // 허락하고자 하는 요청 주소
-  credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
-};
+// const corsOptions = {
+//   origin: 'http://localhost:3000', // 허락하고자 하는 요청 주소
+//   credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
